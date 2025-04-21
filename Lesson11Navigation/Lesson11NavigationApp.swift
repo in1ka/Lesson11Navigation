8b1f42340aadbc23e6e403bc6b5fe9bf5777f109
Inika Bhargava <inikamehro@gmail.com>
2025-04-20 17:41:14 -0700

    Initial Commit

---
+++ Lesson11Navigation/Lesson11NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson11NavigationApp.swift
+//  Lesson11Navigation
+//
+//  Created by Inika Bhargava on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Lesson11NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
